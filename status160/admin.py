ed61dd8f6c93fc66ab5931c541bb4d76823759fb
David McCann <david.a.mccann@gmail.com>
2010-10-27 11:53:59 +0300

    minor fix

--- status160/admin.py
+++ status160/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Comments)
-admin.site.register(models.Warden)
+admin.site.register(models.WardenRelationship)
 admin.site.register(models.Alert)
 
 
