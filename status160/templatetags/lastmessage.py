c4568dc6b575381cd5915b067555dff6955d863d
David McCann <david.a.mccann@gmail.com>
2010-12-09 12:34:43 +0300

    bug fix for last message column

--- status160/templatetags/lastmessage.py
+++ status160/templatetags/lastmessage.py
@@ -6,7 +6,7 @@ import datetime
 def lastmessage(contact):
     try:
         poll = Poll.objects.filter(contacts=contact).exclude(start_date=None).filter(Q(end_date=None) | (~Q(end_date=None) & Q(end_date__gt=datetime.datetime.now()))).latest('start_date')
-        return Reponse.objects.filter(message__connection__contact=contact, poll=poll).latest('message__date').eav.poll_text_value
+        return Response.objects.filter(contact=contact, poll=poll).latest('message__date').message.text
     except:
         return ''
     
