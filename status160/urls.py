ffeaf71f37736cdcf47c79169d91cf73fa02f99a
David McCann <david.a.mccann@gmail.com>
2011-03-14 20:31:26 +0300

    fixed urls

--- status160/urls.py
+++ status160/urls.py
@@ -11,8 +11,7 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-#    url(r'^dashboard/', login_required(dashboard), name='status160-dashboard'),
-    url(r'^dashboard2/$', login_required(generic), {
+    url(r'^dashboard/$', login_required(generic), {
       'model':Contact,
       'filter_forms':[WardenFilterForm, TeamFilterForm, AgencyFilterForm, OfficeFilterForm, FreeSearchForm],
       'action_forms':[MassTextForm, CreateEventForm, SendAlertsForm],
@@ -32,14 +31,10 @@ urlpatterns = patterns(
                  ('',False,'',None,)],
     }, name='status160-dashboard'),
     url(r'^contacts/(?P<pk>\d+)/view/', generic_row, {'model':Contact, 'partial_row':'status160/partials/contact_row.html'}),
-#    url(r'^event/new/', login_required(new_event)),
-#    url(r'^masstext/send/', login_required(send_masstext)),
-#    url(r'^alerts/send/', login_required(send_alerts)),
     url(r'^whitelist/', whitelist),
     url(r'^contacts/(\d+)/delete/', login_required(delete_contact)),
     url(r'^contacts/(\d+)/edit/', login_required(edit_contact)),
     url(r'^contacts/add/', login_required(add_contact)),
-#    url(r'^contacts/(\d+)/view/', login_required(view_contact)),
     url(r'^connections/(\d+)/add/', login_required(add_connection)),
     url(r'^connections/(\d+)/edit/', login_required(edit_connection)),
     url(r'^connections/(\d+)/delete/', login_required(delete_connection)),
