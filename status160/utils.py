bd1198931fdb8681592fcb00a9f5c7b452c609cc
David McCann <david.a.mccann@gmail.com>
2011-11-14 12:41:06 +0300

    updated to yo stuff

--- status160/utils.py
+++ status160/utils.py
@@ -1,14 +1,14 @@
 from rapidsms.models import Backend
 
-PREFIXES = [('70', 'warid'), ('75', 'zain'), ('71', 'utl'), ('', 'dmark')]
+#PREFIXES = [('70', 'warid'), ('75', 'zain'), ('71', 'utl'), ('', 'dmark')]
 
 def assign_backend(number):
     if number.startswith('0'):
         number = '256%s' % number[1:]
-    backendobj = None
-    for prefix, backend in PREFIXES:
-        if number[3:].startswith(prefix):
-            backendobj, created = Backend.objects.get_or_create(name=backend)
-            break
+    backendobj, _ = Backend.objects.get_or_create(name='yo8700')
+#    for prefix, backend in PREFIXES:
+#        if number[3:].startswith(prefix):
+#            backendobj, created = Backend.objects.get_or_create(name=backend)
+#            break
     return (number, backendobj)
 
